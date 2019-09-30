c81fdbc2f44a8e3ffeff655d0032ba696617ae09
Aakriti Singla <aakritisingla18@gmail.com>
2019-09-30 09:31:39 -0700

    nitial status

---
+++ app/src/main/java/com/example/aakritisingla/smarthealthprediction/APIService.java
@@ -0,0 +1,37 @@
+package com.example.aakritisingla.smarthealthprediction;
+
+/*
+ * Created by Shaon on 8/15/16 8:06 PM
+ * Copyright (c) 2016. This is free to use in any software.
+ * You must provide developer name on your project.
+ */
+
+
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
+/**
+ * Created by Shaon on 8/15/2016.
+ */
+public interface APIService {
+
+
+    @FormUrlEncoded
+    @POST("login/views/signup.php")
+    Call<MSG> userSignUp(@Field("name") String name,
+                         @Field("email") String email,
+                         @Field("password") String password);
+
+    @FormUrlEncoded
+    @POST("login/views/login.php")
+    Call<MSG> userLogIn(@Field("email") String email,
+                        @Field("password") String password);
+
+
+
+}
